61b6e7394c56bf2c422791f347ae1320206757d1
Michael T <michaelturner241@gmail.com>
2017-11-13 13:59:41 -0500

    Update jumble.py

--- jumble.py
+++ jumble.py
@@ -1,35 +1,42 @@
-#% python jumble.py  jumble.txt  sowpods.txt –q
-
+#imports mr job
 from mrjob.job import MRJob
 from mrjob.step import MRStep
-import numpy as np
 
+#develops class for anagram unjumbler
 class MR_program(MRJob):	
 	
 	def mapper (self, _,line):
-		#true text and table indicator		
+		#parses out anagrams to be unjumbled 	
 		if "?" in line:
-			destination = 1
 			true = line	
-			line = line[0:-2]					
+			line = line[0:-2]	
+		
+		#parses out words that will be evaluated to see if they are a match
 		else:
-			destination = 0
 			true = line
 			
 		#turns words into themselves 
 		line = ''.join(sorted(list(line)))
 		
+		#yields the word, and its original self( contains ? for analgrams )
 		yield line, true
 	
-	def reducer (self, key, values):	
+	def reducer (self, key, values):
+		#evaluates all values based on key
 		values = [x for x in values]
+		
+		#if there is a key with values longer than one (ie match for jumble) yields anagram and match
 		if len(values) > 1:
 			c1 = values
 			if "?" in c1[0]:
 				yield c1[0], values[1:]
-		else: # our join missed
+		
+		#else, iterates to the next key
+		else: 
 			pass	
 	
 if __name__ == '__main__':
-	#change to match the name of the class
+	#executes mrjob
 	MR_program.run()
+	
+#execute with % python jumble.py  jumble.txt  sowpods.txt –q
